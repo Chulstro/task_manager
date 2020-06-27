2cc9b543d637d44b66b719760eafc6805d45a57e
Chandler Hulstrom <chulstro11@gmail.com>
2020-06-27 16:52:44 -0600

    Trial Period

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,25 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+  end
+
+  def create
+    # binding.pry
+    task = Task.new({
+      title: params[:task][:title],
+      description: params[:task][:description]
+      })
+      # binding.pry
+      task.save
+      # binding.pry
+      redirect_to '/tasks'
+
+      def show
+        binding.pry
+        @task = Task.find(params[:id])
+      end
+  end
+end
